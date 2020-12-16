b13e8bef718c1786d77c70ff01845f124a873909
Chuck Meyer <chuck.meyer@boldpenguin.com>
2020-12-15 21:13:35 -0500

    Day 10 part 1

---
+++ src/day10/day10-1.js
@@ -0,0 +1,37 @@
+'use strict'
+const fs = require('fs')
+
+const getFile = (fileName) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, function (err, data) {
+      if (err) {
+        reject(err.stack)
+      }
+      resolve(data.toString().split('\n').filter(n => n).map(value => parseInt(value)).sort(function(a, b){return a - b}))
+    })
+  })
+}
+
+async function useAllAdapters () {
+  try {
+    let joltage = 0
+    const bucket = {}
+    const adapters = await getFile('input.txt')
+    adapters.push(adapters[adapters.length - 1] + 3)
+    let adapter = 0
+    while ((adapter = adapters.shift()) !== undefined) {
+      const diff = adapter - joltage
+      if (diff <= 3) {
+        bucket[diff] = bucket[diff] ? bucket[diff] + 1 : 1
+        joltage = adapter
+      } else {
+        throw new Error(`No adapter within 3 of ${joltage}.`)
+      }
+    }
+    console.log(bucket['1'] * bucket['3'])
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+useAllAdapters()
--- src/day5/day5-2.js
+++ src/day5/day5-2.js
@@ -1,55 +1,60 @@
-var fs = require('fs');
+'use strict'
+const fs = require('fs')
 
-var getFile = (fileName) => {
+const getFile = (fileName) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(fileName, function(err, data) {
-      resolve(data.toString().split('\n').filter(n => n));
-    });
-})};
+    fs.readFile(fileName, function (err, data) {
+      if (err) {
+        reject(err.stack)
+      }
+      resolve(data.toString().split('\n').filter(n => n))
+    })
+  })
+}
 
-function binRange(low,high,char) {
-  var mid = parseInt((high-low)/2) + low;
-  if(char.match(/F|L/)){
-    return [low,mid];
-  } else if(char.match(/B|R/)){
-    return [mid+1,high];
+function binRange (low, high, char) {
+  const mid = parseInt((high - low) / 2) + low
+  if (char.match(/F|L/)) {
+    return [low, mid]
+  } else if (char.match(/B|R/)) {
+    return [mid + 1, high]
   } else {
-    return null;    
+    return null
   }
 }
 
 function findSeatId(boardingPass){
-  const rowPart = boardingPass.slice(0,7).split('');
-  const colPart = boardingPass.slice(7,10).split('');
-  const rowRange = [0,127];
-  const colRange = [0,7];
+  const rowPart = boardingPass.slice(0,7).split('')
+  const colPart = boardingPass.slice(7,10).split('')
+  const rowRange = [0,127]
+  const colRange = [0,7]
 
-  range = rowRange;
-  rowPart.forEach(char => range = binRange(range[0],range[1],char));
-  var row = range[0];
+  range = rowRange
+  rowPart.forEach(char => range = binRange(range[0],range[1],char))
+  var row = range[0]
 
-  range = colRange;
-  colPart.forEach(char => range = binRange(range[0],range[1],char));
-  var column = range[0];
+  range = colRange
+  colPart.forEach(char => range = binRange(range[0],range[1],char))
+  var column = range[0]
 
-  return parseInt(row*8+column);
+  return parseInt(row*8+column)
 }
 
 async function missingSeatIds() {
   try {
-    var emptySeats = [];
-    var boardingPasses = await getFile('input.txt');
-    seatIds = boardingPasses.map(findSeatId).sort((a,b) => a-b);
-    for (var i=1; i<seatIds.length; i++){
+    var emptySeats = []
+    var boardingPasses = await getFile('input.txt')
+    seatIds = boardingPasses.map(findSeatId).sort((a,b) => a-b)
+    for (var i=1 i<seatIds.length i++){
       console.log(`Comparing ${seatIds[i]} to ${seatIds[i-1]}`)
       if(seatIds[i]-1 != seatIds[i-1]){
-        emptySeats.push(seatIds[i]-1);
+        emptySeats.push(seatIds[i]-1)
       }
     }
-    console.log(emptySeats);
+    console.log(emptySeats)
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
-missingSeatIds();
+missingSeatIds()
